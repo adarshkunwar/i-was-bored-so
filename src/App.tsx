dc0dceb802744295e032c0e30575b63bef6ef8d7
adarshkunwar <adarshkunwarofficial@gmail.com>
2024-12-07 18:07:38 +0545

    refactor: move home page to page/home.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,7 @@
-export default function App() {
-  return (
-    <div className="flex h-screen w-screen flex-col items-center justify-center text-4xl">
-      <div className="">i was</div>
-      <div className="text-8xl">bored</div>
-      <div className="">so</div>
-      <button className="border border-red-500 bg-red-500 text-8xl hover:skew-x-12">
-        i created...
-      </button>
-    </div>
-  );
-}
+import Home from "./page/home";
+
+const App = () => {
+  return <Home />;
+};
+
+export default App;
---
+++ src/page/home.tsx
@@ -0,0 +1,12 @@
+export default function Home() {
+  return (
+    <div className="flex h-screen w-screen flex-col items-center justify-center text-4xl">
+      <div className="">i was</div>
+      <div className="text-8xl">bored</div>
+      <div className="">so</div>
+      <button className="border border-red-500 bg-red-500 text-8xl hover:skew-x-12">
+        i created...
+      </button>
+    </div>
+  );
+}
