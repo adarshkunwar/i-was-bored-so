6d132812d987dbfda9527b75e1bd5bb2bbd29f90
adarshkunwar <adarshkunwarofficial@gmail.com>
2024-12-07 18:05:17 +0545

    feat: home page

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,12 @@
 export default function App() {
   return (
-    <div className="flex w-screen h-screen flex-col items-center justify-center text-4xl">
-      <div className="text-2xl">i was</div>
-      <div>bored</div>
-      <div>so</div>
-      <button>i created...</button>
+    <div className="flex h-screen w-screen flex-col items-center justify-center text-4xl">
+      <div className="">i was</div>
+      <div className="text-8xl">bored</div>
+      <div className="">so</div>
+      <button className="border border-red-500 bg-red-500 text-8xl hover:skew-x-12">
+        i created...
+      </button>
     </div>
   );
 }
